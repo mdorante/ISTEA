9a4e6c57e2ba88d15b1b9582698cd9495d930c5e
mdorante <mdorante10@gmail.com>
2020-08-11 23:33:45 -0300

    Add MyFirstApp (ISTEA Lesson 1)

---
+++ Logical_Programming/Lesson_1/MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program asks the user to input two numbers and prints the sum
+
+            Console.WriteLine("Enter a number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another number:");
+            int num2 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"The sum of these two numbers is: {num1 + num2}");
+        }
+    }
+}
