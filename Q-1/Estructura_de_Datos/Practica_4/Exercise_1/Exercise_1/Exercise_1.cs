0a89473afc149171e603c67e49b7419f5a711795
mdorante <mdorante10@gmail.com>
2020-09-04 14:04:11 -0300

    add Exercise 1

---
+++ Q-1/Estructura_de_Datos/Practica_4/Exercise_1/Exercise_1/Exercise_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int varA = 50;
+            int varB = 99;
+            int buffer;
+
+            Console.WriteLine($"Before swapping values: varA = {varA}, varB = {varB}");
+            buffer = varA;
+            varA = varB;
+            varB = buffer;
+
+            Console.WriteLine($"After swapping: varA = {varA}, varB = {varB}");
+        }
+    }
+}
