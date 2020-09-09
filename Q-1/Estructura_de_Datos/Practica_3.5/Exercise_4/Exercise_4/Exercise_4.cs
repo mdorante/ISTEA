26352984611b35700bceb7d7614242a6bc002447
mdorante <mdorante10@gmail.com>
2020-09-09 10:45:04 -0300

    add exercise 4

---
+++ Q-1/Estructura_de_Datos/Practica_3.5/Exercise_4/Exercise_4/Exercise_4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int fact = 1;
+            bool isNum = false;
+
+            while (!isNum)
+            {
+                Console.WriteLine("Por favor, ingrese un numero:");
+                isNum = int.TryParse(Console.ReadLine(), out number);
+            }
+
+            for (int i = 1; i <= number; i++)
+            {
+                fact *= i;
+            }
+
+            Console.WriteLine($"El factorial de {number} es: {fact}");
+        }
+    }
+}
