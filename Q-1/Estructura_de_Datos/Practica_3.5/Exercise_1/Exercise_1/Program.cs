c4e062956e399bd933b4c6638a963adf90ca0646
mdorante <mdorante10@gmail.com>
2020-09-09 09:38:52 -0300

    add exercise 1

---
+++ Q-1/Estructura_de_Datos/Practica_3.5/Exercise_1/Exercise_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = string.Empty;
+
+            while (word != "chau")
+            {
+                Console.WriteLine("Por favor, ingrese una palabra:");
+                word = Console.ReadLine();
+
+                if (word != "chau")
+                {
+                    Console.WriteLine(word); // para evitar que haga eco de la palabra "chau", ahi deberia directamente salir del loop
+                }
+            }
+
+            Console.WriteLine("Adios!");
+        }
+    }
+}
