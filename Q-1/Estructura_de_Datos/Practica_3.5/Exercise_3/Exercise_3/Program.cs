dc87761f2d6785f24dca730871bc79c76492d9bc
mdorante <mdorante10@gmail.com>
2020-09-09 10:36:59 -0300

    add exercise 3

---
+++ Q-1/Estructura_de_Datos/Practica_3.5/Exercise_3/Exercise_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            while (num1 >= 0 || num2 <= 0)
+            {
+                Console.WriteLine("Ingrese un numero negativo:");
+                int.TryParse(Console.ReadLine(), out num1);
+
+                Console.WriteLine("Ingrese un numero positivo, mayor a 0:");
+                int.TryParse(Console.ReadLine(), out num2);
+            }
+
+            Console.WriteLine($"Los numeros dentro del rango {num1} - {num2} son:");
+
+            for (int i = num1 + 1; i < num2; i++)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
