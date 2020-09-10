acdfc8eee6b094fb4db36f71837ee2c0354df286
mdorante <mdorante10@gmail.com>
2020-09-10 09:29:58 -0300

    add exercise 3

---
+++ Q-1/Estructura_de_Datos/Practica_5/Exercise_3/Exercise_3/Exercise_3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] greetings = { "Hello", "Hola", "Ciao", "Konichiwa", "Hallo" };
+
+            ShowArray(greetings);
+        }
+
+        static void ShowArray(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"[{i}] -> {arr[i]}");
+            }
+        }
+    }
+}
