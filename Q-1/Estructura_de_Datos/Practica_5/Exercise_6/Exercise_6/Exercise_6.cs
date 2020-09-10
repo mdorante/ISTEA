cba87e25c052e8ce8abfc627159520c7fc28d36c
mdorante <mdorante10@gmail.com>
2020-09-10 12:42:24 -0300

    add exercise 6

---
+++ Q-1/Estructura_de_Datos/Practica_5/Exercise_6/Exercise_6/Exercise_6.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Exercise_6.Lib;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bienvenida al juego + evento de batalla
+            Console.WriteLine("\\_|_/ Dungeons and Dragons \\_|_/" + '\n');
+            Console.WriteLine("Te has conseguido a un enemigo!" + '\n');
+
+
+
+            // Atributos del personaje
+            float fuerza = 87f;
+            float agilidad = 62f;
+            float aguante = 74f;
+            float fuerzaDelArma = 79f;
+            float pesoDelArma = 70f;
+            float attack;
+
+            // Generamos aleatoriamente los atributos del enemigo (todos los enemigos van a tener atributos diferentes)
+            float[] enemy = DungeonsEvents.NewEnemy();
+            float aguanteEnemy = enemy[0];
+            float agilidadEnemy = enemy[1];
+            float escudoEnemy = enemy[2];
+            float vidaEnemy = enemy[3];
+            bool enemyAlive;
+
+            // calculamos el attack
+            attack = DungeonsEvents.AttackPoints(fuerza, agilidad, pesoDelArma, fuerzaDelArma, aguante);
+
+            // Calculamos los puntos de defensa del enemigo
+            float defensaEnemy = DungeonsEvents.EnemyDefensePoints(aguanteEnemy, agilidadEnemy, escudoEnemy);
+
+            // Notificamos al jugador los stats del turno
+            Console.WriteLine($"Tus puntos de ataque: {attack}");
+            Console.WriteLine($"Puntos de defensa del enemigo: {defensaEnemy}" + '\n');
+
+            // Calculamos el damage que le infligimos al enemigo
+            float damage = DungeonsEvents.CalculateDamage(attack, defensaEnemy);
+
+            // Atacamos al enemigo
+            enemyAlive = DungeonsEvents.InflictDamage(vidaEnemy, damage);
+
+            Console.WriteLine("[Atacaste al enemigo]" + '\n');
+            Console.WriteLine($"Vida del enemigo: {vidaEnemy}");
+
+            if (!enemyAlive)
+            {
+                Console.WriteLine("Tu enemigo ha muerto.");
+            }
+
+            // Fin del turno
+            Console.WriteLine("Fin del turno.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Q-1/Estructura_de_Datos/Practica_5/Exercise_6/Exercise_6/Lib/DungeonsEvents.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Exercise_6.Lib
+{
+    public class DungeonsEvents
+    {
+        public static float[] NewEnemy()
+        {
+            Random rnd = new Random();
+
+            float[] enemy = new float[4];
+
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                enemy[i] = rnd.Next(1, 100);
+            }
+
+            return enemy;
+        }
+
+        public static float AttackPoints(float fuerza, float agilidad, float pesoDelArma, float fuerzaDelArma, float aguante)
+        {
+            int dadoPunteria = DungeonsEvents.RollDice();
+
+            float attack = (fuerza + agilidad) - (pesoDelArma % aguante);
+
+            if (pesoDelArma % fuerzaDelArma == 0)
+            {
+                attack *= 1.03f;
+            }
+            if (fuerza < pesoDelArma)
+            {
+                attack *= 0.98f;
+            }
+
+            if (dadoPunteria % 2 == 0)
+            {
+                attack += dadoPunteria;
+            }
+            else
+            {
+                attack -= dadoPunteria;
+            }
+
+            return attack;
+        }
+
+        public static float EnemyDefensePoints(float aguante, float agilidad, float escudo)
+        {
+            int dadoSuerte = DungeonsEvents.RollDice();
+
+            if (dadoSuerte >= 5)
+            {
+                escudo = 0;
+            }
+
+            float defensa = (aguante * agilidad) + escudo * 1.5f;
+
+            return defensa;
+        }
+
+        public static float CalculateDamage(float attack, float enemyDefense)
+        {
+            float damage = attack - enemyDefense;
+
+            if (damage < 0)
+            {
+                damage = 0;
+            }
+
+            return damage;
+        }
+
+        public static bool InflictDamage(float enemyLife, float damage)
+        {
+            bool alive = true;
+
+            enemyLife -= damage;
+
+            if (enemyLife <= 0)
+            {
+                alive = false;
+            }
+
+            return alive;
+        }
+
+        private static int RollDice()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 7);
+        }
+    }
+}
