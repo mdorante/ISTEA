401452e6b850a89268c1b881447658bce8772a1b
mdorante <mdorante10@gmail.com>
2020-09-10 09:21:08 -0300

    add exercise 2

---
+++ Q-1/Estructura_de_Datos/Practica_5/Exercise_2/Exercise_2/Exercise_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1 = 129.876;
+            double numero2 = 32.87;
+
+            double suma = Sumar(numero1, numero2);
+
+            Console.WriteLine($"La suma de {numero1} y {numero2} es: {suma}");
+        }
+
+        static double Sumar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
