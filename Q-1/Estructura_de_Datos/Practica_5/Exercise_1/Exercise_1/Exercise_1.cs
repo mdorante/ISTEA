c6edaf866ab957d0e5f13eb9e35ee74e6f37cd2d
mdorante <mdorante10@gmail.com>
2020-09-10 09:11:40 -0300

    add exercise 1

---
+++ Q-1/Estructura_de_Datos/Practica_5/Exercise_1/Exercise_1/Exercise_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, ingrese su nombre:");
+            string name = Console.ReadLine();
+
+            Saludar(name);
+        }
+
+        static void Saludar(string nombre)
+        {
+            Console.WriteLine($"Hola {nombre}");
+        }
+    }
+}
