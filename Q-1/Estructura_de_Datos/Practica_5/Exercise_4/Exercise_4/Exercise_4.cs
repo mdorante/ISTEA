07cc378292d9f2bcfa7ff141d3c2a9aba2ba2882
mdorante <mdorante10@gmail.com>
2020-09-10 09:37:58 -0300

    add exercise 4

---
+++ Q-1/Estructura_de_Datos/Practica_5/Exercise_4/Exercise_4/Exercise_4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2021;
+            bool esBisiesto = EsAnioBisiesto(year);
+
+            if (esBisiesto)
+            {
+                Console.WriteLine($"El año {year} es bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine($"El año {year} no es bisiesto.");
+            }
+        }
+
+        static bool EsAnioBisiesto(int anio)
+        {
+            return (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0));
+        }
+    }
+}
