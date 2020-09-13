2285446938a2060483f2d48fbe0f153626fbb504
mdorante <mdorante10@gmail.com>
2020-09-13 14:51:12 -0300

    refactor exercise 5

--- Q-1/Estructura_de_Datos/Practica_5/Exercise_5/Exercise_5/Exercise_5.cs
+++ Q-1/Estructura_de_Datos/Practica_5/Exercise_5/Exercise_5/Exercise_5.cs
@@ -27,54 +27,13 @@ namespace Exercise_5
             int[] diasDeCadaMes = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
             int cantidad = 0;
 
-            if (EsAnioBisiesto(year))
+            cantidad = diasDeCadaMes[month - 1];
+
+            if (EsAnioBisiesto(year) && month == 2)
             {
-                diasDeCadaMes[1] = 29;
+                cantidad = diasDeCadaMes[month - 1] + 1;
             }
 
-            switch (month)
-            {
-                case 1:
-                    cantidad = diasDeCadaMes[0];
-                    break;
-                case 2:
-                    cantidad = diasDeCadaMes[1];
-                    break;
-                case 3:
-                    cantidad = diasDeCadaMes[2];
-                    break;
-                case 4:
-                    cantidad = diasDeCadaMes[3];
-                    break;
-                case 5:
-                    cantidad = diasDeCadaMes[4];
-                    break;
-                case 6:
-                    cantidad = diasDeCadaMes[5];
-                    break;
-                case 7:
-                    cantidad = diasDeCadaMes[6];
-                    break;
-                case 8:
-                    cantidad = diasDeCadaMes[7];
-                    break;
-                case 9:
-                    cantidad = diasDeCadaMes[8];
-                    break;
-                case 10:
-                    cantidad = diasDeCadaMes[9];
-                    break;
-                case 11:
-                    cantidad = diasDeCadaMes[10];
-                    break;
-                case 12:
-                    cantidad = diasDeCadaMes[11];
-                    break;
-
-                default:
-                    Console.WriteLine("No ha ingresado un numero correspondiente a un mes del año");
-                    break;
-            }
             return cantidad;
         }
     }
