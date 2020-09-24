fb688c34dae789a7ee5eb0c03b94d1379a8f2a11
mdorante <mdorante10@gmail.com>
2020-09-24 14:32:33 -0300

    add Practice 5 exercises 1-3

---
+++ Q-1/Estructura_de_Datos/Practica_5.5/Practica_5.5/Ejercicios_1-4.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Practica_5._5
+{
+    public class Ejercicios_1_4
+    {
+        public static int FindNum(int[] numbers, int num)
+        {
+            int rv = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == num)
+                {
+                    rv = i;
+                    break;
+                }
+            }
+
+            return rv;
+        }
+
+        public static int FindNumIndices(int[] numbers, int[] indices, int num)
+        {
+            int rv = 0;
+            int indicesIndex = 0;
+
+            for (int i = 0; i < indices.Length; i++)
+            {
+                indices[i] = -1;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == num)
+                {
+                    if (indicesIndex < indices.Length)
+                    {
+                        indices[indicesIndex] = i;
+                        indicesIndex++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < indices.Length; i++)
+            {
+                if (indices[i] != -1)
+                {
+                    rv++;
+                }
+            }
+
+            return rv;
+        }
+
+        public static int[,] SumaBidimensional(int[,] arr1, int[,] arr2)
+        {
+            int[,] returnArray = new int[arr1.GetLength(0), arr1.GetLength(1)];
+
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr1.GetLength(1); j++)
+                {
+                    returnArray[i, j] = arr1[i, j] + arr2[i, j];
+                }
+            }
+
+            return returnArray;
+        }
+
+    }
+}
---
+++ Q-1/Estructura_de_Datos/Practica_5.5/Practica_5.5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practica_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Testeamos funciones 1 y 2
+            //int[] testArr = { 1, 4, 3, 4, 5, 6, 4, 2, 4, 5, 6 };
+            //int[] testArr2 = new int[3];
+
+            //Console.WriteLine(Ejercicios_1_4.FindNumIndices(testArr, testArr2, 4));
+
+            //Testeamos funcion 3
+            int[,] array2D1 = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            int[,] array2D2 = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+
+            int[,] sumArray = Ejercicios_1_4.SumaBidimensional(array2D1, array2D2);
+
+            for (int i = 0; i < sumArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < sumArray.GetLength(1); j++)
+                {
+                    Console.Write($"{sumArray[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
