ff6e454398fd9ac19574c1f90df7375e3ede4a75
mdorante <mdorante10@gmail.com>
2020-09-27 19:59:29 -0300

    refactor CStrings EsDigito func

--- Q-1/Estructura_de_Datos/Practica_Cstrings/Cstrings/Cstrings/Program.cs
+++ Q-1/Estructura_de_Datos/Practica_Cstrings/Cstrings/Cstrings/Program.cs
@@ -721,44 +721,7 @@ namespace ConsoleApp3
           */
         static bool EsDigito(char unChar)
         {
-            bool isNum = false;
-
-            switch (unChar)
-            {
-                case '0':
-                    isNum = true;
-                    break;
-                case '1':
-                    isNum = true;
-                    break;
-                case '2':
-                    isNum = true;
-                    break;
-                case '3':
-                    isNum = true;
-                    break;
-                case '4':
-                    isNum = true;
-                    break;
-                case '5':
-                    isNum = true;
-                    break;
-                case '6':
-                    isNum = true;
-                    break;
-                case '7':
-                    isNum = true;
-                    break;
-                case '8':
-                    isNum = true;
-                    break;
-                case '9':
-                    isNum = true;
-                    break;
-
-            }
-
-            return isNum;
+            return unChar >= '0' && unChar <= '9';
         }
 
         static void Test_EsDigito()
