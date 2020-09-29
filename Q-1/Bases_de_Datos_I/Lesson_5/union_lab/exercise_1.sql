c8e724c88dd1e7fe432341e274f5ff46bfe0ea7f
mdorante <mdorante10@gmail.com>
2020-09-28 22:29:12 -0300

    add union lab

---
+++ Q-1/Bases_de_Datos_I/Lesson_5/union_lab/exercise_1.sql
@@ -0,0 +1,14 @@
+-- Mostrar todos los productos vendidos y ordenados
+-- Tablas: Sales.SalesOrderDetail, Production.WorkOrder
+-- Campos: ProductID
+
+    SELECT
+        ProductID
+    FROM
+        Sales.SalesOrderDetail
+UNION
+
+    SELECT
+        ProductID
+    FROM
+        Production.WorkOrder;
